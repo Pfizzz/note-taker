48e69a2f232a14a7f02f49fb02dc39a56c6b537d
Pfizz <pfiste80@gmail.com>
2021-09-04 23:04:00 -0400

    broken post request

--- server.js
+++ server.js
@@ -2,18 +2,57 @@ const express = require('express');
 // instantiate the server
 const PORT = process.env.PORT || 3001;
 const app = express();
+const fs = require('fs');
+const path = require('path');
+app.use(express.static("/public"));
+const uniqid = require('uniqid');
 
-// this will be modularized later
-const { notes } = require('./db/db');
+// middleware to parse incoming data
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
+
+// dont get rid of this
+// const { notes } = require('./db/db');
+
+// get request
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
-    console.log("success");
-  });
+    fs.readFile(path.join(__dirname, './db/db.json'), 'utf-8', function (err, data) {
+        var json = JSON.parse(data);
+        res.json(json);
+        console.log("success get request");
+    });
+});
+
+// POST request
+app.post('/api/notes', (req, res) => {
+
+    fs.readFile(path.join(__dirname, './db/db.json'), 'utf-8', (err, data) => {
+        if (err) {
+            res.status(500).json(err);
+            return;
+        }
+        var json = JSON.parse(data);
+        var note = req.body;
+        note.id = uniqid();
+        json.push(note);
+
+        fs.writeFile(path.join(__dirname, './db/db.json'), 
+        JSON.stringify(json), (err) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log("success!!!!");
+            res.json(note);
+        });
+    });
+});
 
 // start server, goes at bottom
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
-  });
-  
+});
